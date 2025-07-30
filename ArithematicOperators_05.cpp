49930c17bfc3366d62c49b7cc88d2e664dd22e37
Aditi <aditi.gla@gla.ac.in>
2025-07-31 01:29:25 +0530

    % operator

--- ArithematicOperators_05.cpp
+++ ArithematicOperators_05.cpp
@@ -11,16 +11,34 @@
 
 // float
 
-#include<iostream>
-using namespace std;
-int main(){
-    float x=5,y=2;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x=5,y=2;
     
-    cout<<x<<endl;
-    cout<<y<<endl;
-    
-    cout<<x+y<<endl;
-    cout<<x-y<<endl;
-    cout<<x*y<<endl;
-    cout<<x/y<<endl;
-}
+//     cout<<x<<endl;
+//     cout<<y<<endl;
+
+//     cout<<x+y<<endl;
+//     cout<<x-y<<endl;
+//     cout<<x*y<<endl;
+//     cout<<x/y<<endl;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// MODULUS OPERATOR
+
+//1. a%b = a (a<b)
+// 2.a%(-b)=a%b
+// 3.(-a)%b= -[a%b]
+// 4.a%a=0
