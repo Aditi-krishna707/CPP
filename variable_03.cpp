c20f2864139f65ad4f2a87c4b501688f0b0df18c
Aditi <aditi.gla@gla.ac.in>
2025-07-30 23:44:51 +0530

    g

---
+++ variable_03.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;//decleration
+//     x=10;//initilization
+//     int y;
+//     y=90;
+//     cout<<x+y;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     x=20;
+//     y=90;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=10,y=34;
+    cout<<x+y;
+}
