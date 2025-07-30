5fe7c9fc42fe2d1d4d2d35e3afc71b3e93517916
Aditi <aditi.gla@gla.ac.in>
2025-07-31 00:03:04 +0530

    int float

---
+++ ArithematicOperators_05.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=2;
+//     cout<<x+y<<endl;
+//     cout<<x-y<<endl;
+//     cout<<x*y<<endl;
+//     cout<<x/y<<endl;
+// }
+
+
+// float
+
+#include<iostream>
+using namespace std;
+int main(){
+    float x=5,y=2;
+    
+    cout<<x<<endl;
+    cout<<y<<endl;
+    
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+}
