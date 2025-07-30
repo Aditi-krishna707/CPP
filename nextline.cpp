bf25bd4b84c89980f740dc3ea023213c5e2569cd
Aditi <aditi.gla@gla.ac.in>
2025-07-30 23:26:13 +0530

    endl

---
+++ nextline.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello"<<endl;
+//     cout<<"world";
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello";
+//     cout<<endl;
+//     cout<<"world";
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello"<< endl << "world";;
+
+}
+
