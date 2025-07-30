eae78d479f3527a9c9b845cc9e4f3a3f1c23aaf8
Aditi <aditi.gla@gla.ac.in>
2025-07-31 00:27:12 +0530

    x-- --x

---
+++ incrementDecrementOper_06.cpp
@@ -0,0 +1,60 @@
+// post increment
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=9;
+//     x++;
+//     y++;
+//     cout<<x<<endl<<y;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=9;
+//     x--;
+//     y--;
+//     cout<<x<<endl<<y;
+// }
+
+// pre increment
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=9;
+//     ++x;
+//     ++y;
+//     cout<<x<<endl<<y;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=5,y=9;
+//     --x;
+//     --y;
+//     cout<<x<<endl<<y;
+// }
+
+
+
+// +++++++++++++++++++++++IMPORTANT+++++++++++++++++++++++++
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=4;
+//     cout<<x<<endl;
+//     cout<<x++<<endl;
+//     cout<<x<<endl;
+// }
+// +++++++++++++++++++++++++++++++SEE THE DIFFERENCE+++++++++++++++++++++++++
+#include<iostream>
+using namespace std;
+int main(){
+    int x=4;
+    cout<<x<<endl;
+    cout<<++x<<endl;
+    cout<<x<<endl;
+}
