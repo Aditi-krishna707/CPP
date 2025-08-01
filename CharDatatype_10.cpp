a628884aa1b9891375bb58e07e0335f334f7c27b
Aditi <aditi.gla@gla.ac.in>
2025-08-01 08:55:59 +0530

    chardatatype

---
+++ CharDatatype_10.cpp
@@ -0,0 +1,59 @@
+// #include<iostream>
+// using  namespace std;
+// int main(){
+//     char ch='x';
+//     cout<<ch;
+// }
+
+
+// #include<iostream>
+// using  namespace std;
+// int main(){
+//     char ch;
+//     cout<<"enter any char";
+//     cin>>ch;
+//     cout<<ch;
+// }
+
+
+
+// ASCII values
+// a->98  to z->122
+// A->65 to Z -> 90
+/*
+0 -> 48
+1 -> 49
+2 ->50
+.
+.
+.
+.
+.
+9 ->57
+*/
+
+
+
+
+// #include<iostream>
+// using  namespace std;
+// int main(){
+//     char ch='a';
+//     cout<<(int)ch;
+// }
+
+
+// #include<iostream>
+// using  namespace std;
+// int main(){
+//     char ch='A';
+//     cout<<(int)ch;
+// }
+
+
+// #include<iostream>
+// using  namespace std;
+// int main(){
+//     char ch='0';
+//     cout<<(int)ch;
+// }
