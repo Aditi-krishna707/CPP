3b092c12f47584b15300e14a304757fcbe5b267b
Aditi <aditi.gla@gla.ac.in>
2025-08-01 09:22:41 +0530

    ASCII

---
+++ IntToChar_12.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int x=97;
+// char y=(char)x;
+// cout<<y;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int x=36;
+// char y=(char)x;
+// cout<<y;
+// }
+
+
+
+// every char have sky value
